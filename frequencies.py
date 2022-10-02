68eaaedac84717ccf6f6f306a158dd99298a751e
ShakterNano <54268387+ShaksterNano@users.noreply.github.com>
2022-10-02 12:18:10 +0100

    Frequencies solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,16 @@
 """Frequencies function."""
+from collections.abc import Iterable
+from typing import Any
+
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+
+def frequencies(items: Iterable[Any]) -> dict[str, int]:
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        string_item = str(item)
+        if string_item in frequencies:
+            frequencies[string_item] += 1
+        else:
+            frequencies[string_item] = 1
     return frequencies
